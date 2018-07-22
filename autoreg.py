e5f4d64a66bc8423623e9fd816b1009066545be1
deker104 <dshindov16@gmail.com>
2018-07-22 17:07:14 +0600

    Add SendVerif click

--- autoreg.py
+++ autoreg.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import time
 
 driver = webdriver.Firefox()
+driver.get_cookies()
 driver.get('https://e-clius.com/register')
 el = driver.find_element_by_id('login_user')
 el.send_keys('nickname')
@@ -10,5 +13,14 @@ el = driver.find_element_by_id('ob_sogl')
 el.click()
 el = driver.find_element_by_id('Sub')
 el.click()
+while True:
+    try:
+        el = driver.find_element_by_id('SendVerif')
+    except NoSuchElementException:
+        el = None
+    if el:
+        el = el.find_element_by_xpath('.//span')
+        el.click()
+        break
 input('')
 driver.close()
